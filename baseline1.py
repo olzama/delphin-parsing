9b10a6b20abefe475b98b3e9ba2573dadcf902d2
Olga Zamaraeva <olzama@uw.edu>
2023-08-29 15:38:18 +0200

    no message

--- baseline1.py
+++ baseline1.py
@@ -6,7 +6,7 @@ This baseline is the time needed to parse a set of sentences using the ACE parse
 '''
 
 import glob
-from ace_wrapper import run_ace
+from ace_wrapper import run_ace_on_ts
 
 #class baseline1:
 
@@ -14,8 +14,9 @@ def run(profiles_path, grammar, ace_exec, output_path):
     print('Running baseline 1 (no supertagging)...')
     responses = []
     for i, tsuite in enumerate(sorted(glob.iglob(profiles_path + '/**'))):
-        responses_tsuite, coverage, avg_time = run_ace(tsuite, grammar, ace_exec, ["-1"], 'i-input', output_path)
-        responses.extend(responses_tsuite)
-        print('Coverage for ' + tsuite + ': ' + str(coverage))
-        print('Average parsing time per sentence time for ' + tsuite + ': ' + str(avg_time))
+        run_ace_on_ts(tsuite, grammar, ace_exec, ['-1'], 'i-input', output_path)
+        #responses_tsuite, coverage, avg_time = run_ace(tsuite, grammar, ace_exec, ["-1"], 'i-input', output_path)
+        #responses.extend(responses_tsuite)
+        #print('Coverage for ' + tsuite + ': ' + str(coverage))
+        #print('Average parsing time per sentence time for ' + tsuite + ': ' + str(avg_time))
 
